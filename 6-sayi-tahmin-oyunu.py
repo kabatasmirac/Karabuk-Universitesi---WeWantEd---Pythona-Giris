389eb500cf96bc12e15593fb787e9f264f2128e5
Mirac Kabatas <kabatasmirac0@gmail.com>
2017-03-26 21:57:00 +0200

    Update 6-sayi-tahmin-oyunu.py

--- 6-sayi-tahmin-oyunu.py
+++ 6-sayi-tahmin-oyunu.py
@@ -1,11 +1,12 @@
 import random
 sayi = random.randint(1,100)
 print ("Tahmin Oyununa Hos Geldiniz")
-sayac=1
+sayac=0
 
 while True:
     tahmin = int(input("Sayi Girin:"))
-
+    sayac += 1
+    
     if tahmin == sayi:
         print ("\nTebrikler {} denemede bildiniz!" .format(sayac))
         break
